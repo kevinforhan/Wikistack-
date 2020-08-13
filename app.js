cd4e810a4912ec4573acba95eca8db952e9a80e4
Kevin Forhan <kevin4forhan@gmail.com>
2020-08-13 14:15:23 -0500

    wikistack

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const layout = require("/views/layout"); 
+
+const app = express();
+
+app.use(morgan("dev"))
+app.use(express.static(__dirname + "/public"))
+app.use(express.urlencoded({ extended: false }))
+
+
+app.get("/", (req, res, next) => {
+    res.send('Hello world')
+})
+
+app.get("/", (req, res, next) => {
+    res.send(layout(''))
+})
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`)
+});
